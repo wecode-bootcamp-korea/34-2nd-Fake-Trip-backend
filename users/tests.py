f1844c672be73d8df8fae7ca6a16c579c1ab4167
dingwan <dingwan0331@gmail.com>
2022-07-06 00:26:34 +0900

    Create : users.views.SigninView & users.tests
    - SigninView 구현
    - core.social_apis 로 KakaoAPI 핸들링
    - tests.py 작성

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,121 @@
-from django.test import TestCase
+import jwt
 
-# Create your tests here.
+from unittest.mock import patch, MagicMock
+from django.test   import TestCase, Client
+
+from .models           import User
+from faketrip.settings import SECRET_KEY, ALGORITHM
+
+class SinginTest(TestCase):
+    def setUp(self):
+        User.objects.create(
+            id       = 1,
+            kakao_pk = 151512,
+            email    = 'qwer123@nate.com',
+            name     = '위코드'
+        )
+
+    def tearDown(self):
+        User.objects.all().delete()
+
+    @patch("core.social_apis.requests")
+    def test_success_kakao_signin(self, mocked_requests):
+        client = Client()
+
+        class MockedResponse:
+            def json(self):
+                return {
+                    'id': 151512, 
+                    'connected_at': '2022-07-06T16:22:22Z', 
+                    'properties': {
+                        'nickname': '위코드', 
+                        'profile_image': 'http://가짜.이미지.jpg', 
+                        'thumbnail_image': 'http://가짜.이미지2.jpg'
+                        }, 
+                    'kakao_account': {
+                        'profile_nickname_needs_agreement': False, 
+                        'profile_image_needs_agreement': False, 
+                        'profile': {
+                            'nickname': '위코드', 
+                            'thumbnail_image_url': 'http://가짜.이미지2.jpg', 
+                            'profile_image_url': 'http://가짜.이미지.jpg', 
+                            'is_default_image': False}, 
+                            'has_email': True, 
+                            'email_needs_agreement': False, 
+                            'is_email_valid': True, 
+                            'is_email_verified': True, 
+                            'email': 'qwer123@nate.com'}}
+
+            status_code = 200
+    
+        mocked_requests.get = MagicMock(return_value = MockedResponse())
+        headers             = {'HTTP_token' : '가짜 access_token'}
+        response            = client.get('/users/signin', **headers)
+        user                = User.objects.get(id = 1)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.headers.get('Authorization'),
+            jwt.encode({'user_id' : user.id}, SECRET_KEY, ALGORITHM)
+        )
+        self.assertEqual(response.json()['message'], 'SUCCESS')
+        
+    @patch('core.social_apis.requests')
+    def test_success_changed_kakao_information_user_signin(self, mocked_requests):
+        client = Client()
+
+        class MockedResponse:
+            def json(self):
+                return {
+                    'id': 151512, 
+                    'connected_at': '2022-07-06T16:22:22Z', 
+                    'properties': {
+                        'nickname': '코드', 
+                        'profile_image': 'http://가짜.이미지.jpg', 
+                        'thumbnail_image': 'http://가짜.이미지2.jpg'
+                        }, 
+                    'kakao_account': {
+                        'profile_nickname_needs_agreement': False, 
+                        'profile_image_needs_agreement': False, 
+                        'profile': {
+                            'nickname': '위코드', 
+                            'thumbnail_image_url': 'http://가짜.이미지2.jpg', 
+                            'profile_image_url': 'http://가짜.이미지.jpg', 
+                            'is_default_image': False}, 
+                            'has_email': True, 
+                            'email_needs_agreement': False, 
+                            'is_email_valid': True, 
+                            'is_email_verified': True, 
+                            'email': 'qwer@nate.com'}}
+    
+            status_code = 200
+        
+        mocked_requests.get = MagicMock(return_value = MockedResponse())
+        headers             = {'HTTP_token' : '가짜 access_token'}
+        response            = client.get('/users/signin', **headers)
+        user                = User.objects.get(id = 1)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.headers.get('Authorization'),
+            jwt.encode({'user_id' : user.id}, SECRET_KEY, ALGORITHM)
+        )
+        self.assertEqual(response.json()['message'], 'SUCCESS')
+        self.assertEqual(user.name, '코드')
+        self.assertEqual(user.email, 'qwer@nate.com')
+
+    @patch("core.social_apis.requests")
+    def test_fail_kakao_signin(self, mocked_requests):
+        client = Client()
+
+        class MockedResponse:
+            def json(self):
+                return {'msg': 'this access token does not exist', 'code': -401}
+                
+            status_code = 401
+    
+        mocked_requests.get = MagicMock(return_value = MockedResponse())
+        headers             = {'HTTP_token' : '가짜 access_token'}
+        response            = client.get('/users/signin', **headers)
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.headers.get('Authorization'),None)
+        self.assertEqual(response.json()['message'], 'Invalid Token')
--- users/views.py
+++ users/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+import requests
 
-# Create your views here.
+import jwt
+
+from django.views import View
+from django.http  import JsonResponse
+
+from users.models      import User
+from faketrip.settings import SECRET_KEY, ALGORITHM
+from core.social_apis  import Kakao_Token_Error, KakaoAPI
+
+class SigninView(View):
+    def get(self, request):
+        try:
+            access_token     = request.headers.get('Authorization')
+            user_information = KakaoAPI(access_token).get_kakao_user_information()
+            
+            kakao_pk = user_information.get('id')
+            email    = user_information.get('kakao_account').get('email')
+            nickname = user_information.get('properties').get('nickname')
+
+            user, created = User.objects.get_or_create(kakao_pk=kakao_pk, default = {"email" : email, "name" : nickname})
+            
+            if not created and not(user.email == email and user.name == nickname):
+                user.email, user.name = email, nickname
+                user.save()
+
+            authorization = jwt.encode({'user_id' : user.id}, SECRET_KEY, ALGORITHM)
+
+            return JsonResponse({'message' : 'SUCCESS'},headers = {'Authorization' : authorization}, status = 200)
+        
+        except Kakao_Token_Error as error:
+            return JsonResponse({'message' : error.message}, status = 401)
