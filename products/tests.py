5adaba3d685fbf749a419c9c0a6ca6d7204677b6
minha <miraculum95@gmail.com>
2022-07-08 15:09:32 +0900

    Fix: conflict
    Delete: delete check_in, check_out Q

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,143 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+from products.models import Category, Region, Product, Room, ProductImage
+
+class ProductListTest(TestCase):
+    def setUp(self):
+        Category.objects.create(
+            id = 1,
+            name = '호텔'
+        )
+        Region.objects.create(
+            id = 1,
+            name = '제주도'
+        )
+        Product.objects.bulk_create([
+            Product(id = 1,
+            name = '호텔1',
+            grade = 5,
+            check_in = '14:00',
+            check_out = '11:00',
+            address = '호텔1 주소',
+            latitude = 33.2406823000,
+            longtitude = 126.5317835000,
+            region_id = 1,
+            category_id = 1),
+            Product(id = 2,
+            name = '호텔2',
+            grade = 5,
+            check_in = '12:00',
+            check_out = '11:00',
+            address = '호텔2 주소',
+            latitude = 33.2406820000,
+            longtitude = 126.5317805000,
+            region_id = 1,
+            category_id = 1),
+        ])
+        Room.objects.bulk_create([
+            Room(name='호텔1방1', product_id='1', price='150000', size='15'),
+            Room(name='호텔1방2', product_id='1', price='180000', size='18'),
+            Room(name='호텔1방3', product_id='1', price='210000', size='28'),
+            Room(name='호텔1방4', product_id='1', price='300000', size='30'),
+            Room(name='호텔2방1', product_id='2', price='80000', size='11'),
+            Room(name='호텔2방2', product_id='2', price='180000', size='21'),
+            Room(name='호텔2방3', product_id='2', price='200000', size='22'),
+            Room(name='호텔2방4', product_id='2', price='250000', size='26')
+        ])
+        ProductImage.objects.bulk_create([
+            ProductImage(id = 1, url = 'https://www.naver.com/', is_main = 1, product_id = 1),
+            ProductImage(id = 2, url = 'https://www.daum.net/', is_main = 1, product_id = 2)
+        ])
+
+    
+    def tearDown(self):
+        Category.objects.all().delete
+        Region.objects.all().delete
+        Product.objects.all().delete
+    
+    def test_product_list_post(self):
+        client = Client()
+        response = client.get('/products')
+        self.assertEqual(response.json(),
+        {
+            "results": [
+                {
+                    "id": 1,
+                    "name": "호텔1",
+                    "grade": 5,
+                    "address": "호텔1 주소",
+                    "region": "제주도",
+                    "check_in": "14:00",
+                    "check_out": "11:00",
+                    "category": "호텔",
+                    "main_image": "https://www.naver.com/",
+                    "price": {
+                        "min_price": "150000.00",
+                        "max_price": "300000.00",
+                        "low_price": 150000,
+                        "high_price": 300000
+                    }
+                },
+                {
+                    "id": 2,
+                    "name": "호텔2",
+                    "grade": 5,
+                    "address": "호텔2 주소",
+                    "region": "제주도",
+                    "check_in": "12:00",
+                    "check_out": "11:00",
+                    "category": "호텔",
+                    "main_image": "https://www.daum.net/",
+                    "price": {
+                        "min_price": "80000.00",
+                        "max_price": "250000.00",
+                        "low_price": 80000,
+                        "high_price": 250000
+                    }
+                }]
+        })                
+        self.assertEqual(response.status_code, 200)
+
+    def test_product_check_in_ascending_post(self):
+        client = Client()
+        response = client.get('/products?sort=check_in-ascending')
+        self.assertEqual(response.json(),
+        {
+            "results": [
+                {
+                    "id": 2,
+                    "name": "호텔2",
+                    "grade": 5,
+                    "address": "호텔2 주소",
+                    "region": "제주도",
+                    "check_in": "12:00",
+                    "check_out": "11:00",
+                    "category": "호텔",
+                    "main_image": "https://www.daum.net/",
+                    "price": {
+                        "min_price": "80000.00",
+                        "max_price": "250000.00",
+                        "low_price": 80000,
+                        "high_price": 250000
+                    }
+                },
+                {
+                    "id": 1,
+                    "name": "호텔1",
+                    "grade": 5,
+                    "address": "호텔1 주소",
+                    "region": "제주도",
+                    "check_in": "14:00",
+                    "check_out": "11:00",
+                    "category": "호텔",
+                    "main_image": "https://www.naver.com/",
+                    "price": {
+                        "min_price": "150000.00",
+                        "max_price": "300000.00",
+                        "low_price": 150000,
+                        "high_price": 300000
+                    }
+                }
+                ]
+        })                
+        self.assertEqual(response.status_code, 200)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from django.http      import JsonResponse
+from django.views     import View
+from django.db.models import Q
+from django.db.models import Min, Max, IntegerField
 
-# Create your views here.
+from products.models import Product
+from orders.models import Reservation
+
+class ProductListView(View):
+    def get(self, request):
+        products = ['name', 'region', 'grade', 'check_in', 'check_out']
+        for product in products:
+           globals()["{}".format(product)] = request.GET.get(product, None)
+
+        price = request.GET.get('price', 0)
+        sort   = request.GET.get('sort')
+        offset = int(request.GET.get('offset', 0))
+        limit  = int(request.GET.get('limit', 100))
+
+        q = Q()
+
+        if name:
+            q &= Q(name = name)
+
+        if region:
+            q &= Q(region__name = region)
+
+        if grade:
+            q &= Q(grade = grade)
+
+        if price:
+            q &= Q(room__price__gte = int(price))
+
+        sort_set = {
+            'random' : '?',
+            'check_in-ascending' : 'check_in',
+            'price-asceding' : 'room__price',
+        }
+
+        order_key = sort_set.get(sort, 'id')
+        
+        products = Product.objects.filter(q).order_by(order_key)[offset:offset+limit]
+        
+        results = [{
+                'id' : product.id,
+                'name' : product.name,
+                'grade' : product.grade,
+                'address' : product.address,
+                'region' : product.region.name,
+                'check_in' : product.check_in,
+                'check_out' : product.check_out,
+                'category' : product.category.name,
+                'main_image': product.productimage_set.get(is_main=1).url,
+                'price' : {
+                    'min_price' : product.room_set.aggregate(min = Min('price'))['min'],
+                    'max_price' : product.room_set.aggregate(max = Max('price'))['max'],
+                    'low_price' : product.room_set.filter(price__gte=price).aggregate(low_price = Min('price', output_field=IntegerField()))['low_price'],
+                    'high_price' : product.room_set.filter(price__gte=price).aggregate(high_price = Max('price', output_field=IntegerField()))['high_price']
+                }
+            } for product in products
+            ]
+        return JsonResponse({'results' : results}, status = 200)
+
