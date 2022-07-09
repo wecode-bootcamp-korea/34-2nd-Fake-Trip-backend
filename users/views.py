d8e8129d76367da6dce3be20b83d49894da39ecd
dingwan <dingwan0331@gmail.com>
2022-07-09 14:03:31 +0900

    Refactor : users.views.SigninView
    - models.py 변경으로 인해 get_or_create 시 phone_number 에 None 값을 추가하였습니다

--- users/views.py
+++ users/views.py
@@ -15,11 +15,12 @@ class SigninView(View):
             access_token     = request.headers.get('Authorization')
             user_information = KakaoAPI(access_token).get_kakao_user_information()
             
-            kakao_pk = user_information.get('id')
-            email    = user_information.get('kakao_account').get('email')
-            nickname = user_information.get('properties').get('nickname')
+            kakao_pk      = user_information.get('id')
+            email         = user_information.get('kakao_account').get('email')
+            nickname      = user_information.get('properties').get('nickname')
+            phone_number = user_information.get('properties').get('phone_numeber')
 
-            user, created = User.objects.get_or_create(kakao_pk=kakao_pk, default = {"email" : email, "name" : nickname})
+            user, created = User.objects.get_or_create(kakao_pk=kakao_pk, defaults = {"email" : email, "name" : nickname, 'phone_number' : phone_number})
             
             if not created and not(user.email == email and user.name == nickname):
                 user.email, user.name = email, nickname
