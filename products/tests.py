28eddf64fed5323f3df20c4b01c3f85a38dcdb48
minha <miraculum95@gmail.com>
2022-07-11 15:41:30 +0900

    Update: add datas & searching method
    Delete: price data
    
    Delete: price data
    
    Update: merge

--- products/tests.py
+++ products/tests.py
@@ -39,72 +39,56 @@ class ProductListTest(TestCase):
             category_id = 1),
         ])
         Room.objects.bulk_create([
-            Room(name='호텔1방1', product_id='1', price='150000', size='15'),
-            Room(name='호텔1방2', product_id='1', price='180000', size='18'),
-            Room(name='호텔1방3', product_id='1', price='210000', size='28'),
-            Room(name='호텔1방4', product_id='1', price='300000', size='30'),
-            Room(name='호텔2방1', product_id='2', price='80000', size='11'),
-            Room(name='호텔2방2', product_id='2', price='180000', size='21'),
-            Room(name='호텔2방3', product_id='2', price='200000', size='22'),
-            Room(name='호텔2방4', product_id='2', price='250000', size='26')
+            Room(id = 1, name='호텔1방1', product_id='1', price='150000', size='15'),
+            Room(id = 2, name='호텔1방2', product_id='1', price='180000', size='18'),
+            Room(id = 3, name='호텔1방3', product_id='1', price='210000', size='28'),
+            Room(id = 4, name='호텔1방4', product_id='1', price='300000', size='30'),
+            Room(id = 5, name='호텔2방1', product_id='2', price='80000', size='11'),
+            Room(id = 6, name='호텔2방2', product_id='2', price='180000', size='21'),
+            Room(id = 7, name='호텔2방3', product_id='2', price='200000', size='22'),
+            Room(id = 8, name='호텔2방4', product_id='2', price='250000', size='26')
         ])
         ProductImage.objects.bulk_create([
             ProductImage(id = 1, url = 'https://www.naver.com/', is_main = 1, product_id = 1),
             ProductImage(id = 2, url = 'https://www.daum.net/', is_main = 1, product_id = 2)
         ])
+        Amenity.objects.create(id=1, name='수영장')
 
+        #예약 관련 user와 reservation
+        User.objects.create(id=1, 
+        created_at='2022-07-07 20:35:56.636309', 
+        updated_at='2022-07-07 20:35:56.636395', 
+        name='김유저', 
+        email='user@gmail.com', 
+        kakao_pk='2330551369'
+        )
+        Reservation.objects.bulk_create([
+            Reservation(id=1, 
+            guest_information={'name':'김유저', 'email':'user@gmail.com'}, 
+            start_date='2022-07-01', 
+            end_date='2022-07-05',
+            room_id=1, user_id=1),
+            Reservation(id=2, 
+            guest_information={'name':'김유저', 'email':'user@gmail.com'}, 
+            start_date='2022-07-15', 
+            end_date='2022-07-22',
+            room_id=2, user_id=1)
+        ])
+        Review.objects.bulk_create([
+            Review(id=1, content= '별루', rating='2', product_id=1, room_id=1, user_id=1),
+            Review(id=2, content= '좋아요!', rating='4', product_id=1, room_id=1, user_id=1),
+            Review(id=3, content= '또 가고싶음', rating='5', product_id=2, room_id=5, user_id=1),
+            Review(id=4, content= '다시왔다', rating='3', product_id=2, room_id=5, user_id=1)
+        ])
     
     def tearDown(self):
         Category.objects.all().delete
         Region.objects.all().delete
         Product.objects.all().delete
-    
-    def test_product_list_post(self):
-        client = Client()
-        response = client.get('/products')
-        self.assertEqual(response.json(),
-        {
-            "results": [
-                {
-                    "id": 1,
-                    "name": "호텔1",
-                    "grade": 5,
-                    "address": "호텔1 주소",
-                    "region": "제주도",
-                    "check_in": "14:00",
-                    "check_out": "11:00",
-                    "category": "호텔",
-                    "main_image": "https://www.naver.com/",
-                    "price": {
-                        "min_price": "150000.00",
-                        "max_price": "300000.00",
-                        "low_price": 150000,
-                        "high_price": 300000
-                    }
-                },
-                {
-                    "id": 2,
-                    "name": "호텔2",
-                    "grade": 5,
-                    "address": "호텔2 주소",
-                    "region": "제주도",
-                    "check_in": "12:00",
-                    "check_out": "11:00",
-                    "category": "호텔",
-                    "main_image": "https://www.daum.net/",
-                    "price": {
-                        "min_price": "80000.00",
-                        "max_price": "250000.00",
-                        "low_price": 80000,
-                        "high_price": 250000
-                    }
-                }]
-        })                
-        self.assertEqual(response.status_code, 200)
 
     def test_product_check_in_ascending_post(self):
         client = Client()
-        response = client.get('/products?sort=check_in-ascending')
+        response = client.get('/products?start_date=2022-05-01&end_date=2022-05-03&price=200000&sort=check_in-ascending')
         self.assertEqual(response.json(),
         {
             "results": [
@@ -116,13 +100,20 @@ class ProductListTest(TestCase):
                     "region": "제주도",
                     "check_in": "12:00",
                     "check_out": "11:00",
+                    "latitude" : "33.2406820000",
+                    "longtitude" : "126.5317805000",
                     "category": "호텔",
                     "main_image": "https://www.daum.net/",
+                    "amanity": [
+                        "수영장"
+                    ],
+                    "avg_rate": 4.0,
                     "price": {
                         "min_price": "80000.00",
                         "max_price": "250000.00",
-                        "low_price": 80000,
-                        "high_price": 250000
+                        "low_price": 360000,
+                        "high_price": 500000,
+                        "date": 2
                     }
                 },
                 {
@@ -135,11 +126,16 @@ class ProductListTest(TestCase):
                     "check_out": "11:00",
                     "category": "호텔",
                     "main_image": "https://www.naver.com/",
+                    "amanity": [
+                        "수영장"
+                    ],
+                    "avg_rate": 3.0,
                     "price": {
-                        "min_price": "150000.00",
-                        "max_price": "300000.00",
-                        "low_price": 150000,
-                        "high_price": 300000
+                        "min_price": 150000,
+                        "max_price": 300000,
+                        "low_price": 300000,
+                        "high_price": 600000,
+                        "date": 2
                     }
                 }
                 ]
