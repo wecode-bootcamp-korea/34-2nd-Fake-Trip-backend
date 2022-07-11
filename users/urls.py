a9f3d869e53eb5130e1a58fae80d983d7690d649
dingwan <dingwan0331@gmail.com>
2022-07-11 20:13:53 +0900

    Create : users.UserView
    -핸드폰 번호 수정기능 구현

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from users.views import SigninView
+from users.views import SigninView, UserView
 urlpatterns = [
-    path('/signin', SigninView.as_view())
+    path('/signin', SigninView.as_view()),
+    path('', UserView.as_view())
 ]
