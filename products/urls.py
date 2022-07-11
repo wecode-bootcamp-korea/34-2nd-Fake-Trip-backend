e7587a6fb980c63472b9479dca17f695823e6ac5
dingwan <dingwan0331@gmail.com>
2022-07-11 20:34:49 +0900

    Create : products ReviewsView & tests.py

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from products.views import ProductListView, ProductView
+from products.views import ProductListView, ProductView, ReviewsView
 
 urlpatterns = [
     path('', ProductListView.as_view()),
-    path('/<int:product_id>', ProductView.as_view())
+    path('/<int:product_id>', ProductView.as_view()),
+    path('/<int:product_id>/reviews', ReviewsView.as_view())
 ]
