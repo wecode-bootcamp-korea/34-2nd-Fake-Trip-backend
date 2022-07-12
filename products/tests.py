a62fe9ea061366a6c6f0979d968d00c09761ec68
dingwan <dingwan0331@gmail.com>
2022-07-12 22:37:16 +0900

    Add : products.tests.ProductTest

--- products/tests.py
+++ products/tests.py
@@ -4,146 +4,145 @@ from django.test import TestCase, Client
 
 from products.models   import Category, Region, Product, Room, ProductImage
 from users.models      import User, Review
-from orders.models     import Reservation
+from orders.models     import Reservation, ReservationStatus
 from faketrip.settings import SECRET_KEY, ALGORITHM
 
-# class ProductListTest(TestCase):
-#     def setUp(self):
-#         Category.objects.create(
-#             id = 1,
-#             name = '호텔'
-#         )
-#         Region.objects.create(
-#             id = 1,
-#             name = '제주도'
-#         )
-#         Product.objects.bulk_create([
-#             Product(id = 1,
-#             name = '호텔1',
-#             grade = 5,
-#             check_in = '14:00',
-#             check_out = '11:00',
-#             address = '호텔1 주소',
-#             latitude = 33.2406823000,
-#             longtitude = 126.5317835000,
-#             region_id = 1,
-#             category_id = 1),
-#             Product(id = 2,
-#             name = '호텔2',
-#             grade = 5,
-#             check_in = '12:00',
-#             check_out = '11:00',
-#             address = '호텔2 주소',
-#             latitude = 33.2406820000,
-#             longtitude = 126.5317805000,
-#             region_id = 1,
-#             category_id = 1),
-#         ])
-#         Room.objects.bulk_create([
-#             Room(id = 1, name='호텔1방1', product_id='1', price='150000', size='15'),
-#             Room(id = 2, name='호텔1방2', product_id='1', price='180000', size='18'),
-#             Room(id = 3, name='호텔1방3', product_id='1', price='210000', size='28'),
-#             Room(id = 4, name='호텔1방4', product_id='1', price='300000', size='30'),
-#             Room(id = 5, name='호텔2방1', product_id='2', price='80000', size='11'),
-#             Room(id = 6, name='호텔2방2', product_id='2', price='180000', size='21'),
-#             Room(id = 7, name='호텔2방3', product_id='2', price='200000', size='22'),
-#             Room(id = 8, name='호텔2방4', product_id='2', price='250000', size='26')
-#         ])
-#         ProductImage.objects.bulk_create([
-#             ProductImage(id = 1, url = 'https://www.naver.com/', is_main = 1, product_id = 1),
-#             ProductImage(id = 2, url = 'https://www.daum.net/', is_main = 1, product_id = 2)
-#         ])
-#         Amenity.objects.create(id=1, name='수영장')
-
-#         #예약 관련 user와 reservation
-#         User.objects.create(id=1, 
-#         created_at='2022-07-07 20:35:56.636309', 
-#         updated_at='2022-07-07 20:35:56.636395', 
-#         name='김유저', 
-#         email='user@gmail.com', 
-#         kakao_pk='2330551369'
-#         )
-#         Reservation.objects.bulk_create([
-#             Reservation(id=1, 
-#             guest_information={'name':'김유저', 'email':'user@gmail.com'}, 
-#             start_date='2022-07-01', 
-#             end_date='2022-07-05',
-#             room_id=1, user_id=1),
-#             Reservation(id=2, 
-#             guest_information={'name':'김유저', 'email':'user@gmail.com'}, 
-#             start_date='2022-07-15', 
-#             end_date='2022-07-22',
-#             room_id=2, user_id=1)
-#         ])
-#         Review.objects.bulk_create([
-#             Review(id=1, content= '별루', rating='2', product_id=1, room_id=1, user_id=1),
-#             Review(id=2, content= '좋아요!', rating='4', product_id=1, room_id=1, user_id=1),
-#             Review(id=3, content= '또 가고싶음', rating='5', product_id=2, room_id=5, user_id=1),
-#             Review(id=4, content= '다시왔다', rating='3', product_id=2, room_id=5, user_id=1)
-#         ])
-    
-#     def tearDown(self):
-#         Category.objects.all().delete
-#         Region.objects.all().delete
-#         Product.objects.all().delete
-
-#     def test_product_check_in_ascending_post(self):
-#         client = Client()
-#         response = client.get('/products?start_date=2022-05-01&end_date=2022-05-03&price=200000&sort=check_in-ascending')
-#         self.assertEqual(response.json(),
-#         {
-#             "results": [
-#                 {
-#                     "id": 2,
-#                     "name": "호텔2",
-#                     "grade": 5,
-#                     "address": "호텔2 주소",
-#                     "region": "제주도",
-#                     "check_in": "12:00",
-#                     "check_out": "11:00",
-#                     "latitude" : "33.2406820000",
-#                     "longtitude" : "126.5317805000",
-#                     "category": "호텔",
-#                     "main_image": "https://www.daum.net/",
-#                     "amanity": [
-#                         "수영장"
-#                     ],
-#                     "avg_rate": 4.0,
-#                     "price": {
-#                         "min_price": "80000.00",
-#                         "max_price": "250000.00",
-#                         "low_price": 360000,
-#                         "high_price": 500000,
-#                         "date": 2
-#                     }
-#                 },
-#                 {
-#                     "id": 1,
-#                     "name": "호텔1",
-#                     "grade": 5,
-#                     "address": "호텔1 주소",
-#                     "region": "제주도",
-#                     "check_in": "14:00",
-#                     "check_out": "11:00",
-#                     "category": "호텔",
-#                     "main_image": "https://www.naver.com/",
-#                     "amanity": [
-#                         "수영장"
-#                     ],
-#                     "avg_rate": 3.0,
-#                     "price": {
-#                         "min_price": 150000,
-#                         "max_price": 300000,
-#                         "low_price": 300000,
-#                         "high_price": 600000,
-#                         "date": 2
-#                     }
-#                 }
-#                 ]
-#         })                
-#         self.assertEqual(response.status_code, 200)
-
-
+class ProductListTest(TestCase):
+    def setUp(self):
+        Category.objects.create(
+            id = 1,
+            name = '호텔'
+        )
+        Region.objects.create(
+            id = 1,
+            name = '제주도'
+        )
+        Product.objects.bulk_create([
+            Product(id = 1,
+            name = '호텔1',
+            grade = 5,
+            check_in = '14:00',
+            check_out = '11:00',
+            address = '호텔1 주소',
+            latitude = 33.2406823000,
+            longtitude = 126.5317835000,
+            region_id = 1,
+            category_id = 1),
+            Product(id = 2,
+            name = '호텔2',
+            grade = 5,
+            check_in = '12:00',
+            check_out = '11:00',
+            address = '호텔2 주소',
+            latitude = 33.2406820000,
+            longtitude = 126.5317805000,
+            region_id = 1,
+            category_id = 1),
+        ])
+        Room.objects.bulk_create([
+            Room(id = 1, name='호텔1방1', product_id='1', price='150000', size='15'),
+            Room(id = 2, name='호텔1방2', product_id='1', price='180000', size='18'),
+            Room(id = 3, name='호텔1방3', product_id='1', price='210000', size='28'),
+            Room(id = 4, name='호텔1방4', product_id='1', price='300000', size='30'),
+            Room(id = 5, name='호텔2방1', product_id='2', price='80000', size='11'),
+            Room(id = 6, name='호텔2방2', product_id='2', price='180000', size='21'),
+            Room(id = 7, name='호텔2방3', product_id='2', price='200000', size='22'),
+            Room(id = 8, name='호텔2방4', product_id='2', price='250000', size='26')
+        ])
+        ProductImage.objects.bulk_create([
+            ProductImage(id = 1, url = 'https://www.naver.com/', is_main = 1, product_id = 1),
+            ProductImage(id = 2, url = 'https://www.daum.net/', is_main = 1, product_id = 2)
+        ])
+        Amenity.objects.create(id=1, name='수영장')
+# 
+        #예약 관련 user와 reservation
+        User.objects.create(id=1, 
+        created_at='2022-07-07 20:35:56.636309', 
+        updated_at='2022-07-07 20:35:56.636395', 
+        name='김유저', 
+        email='user@gmail.com', 
+        kakao_pk='2330551369'
+        )
+        Reservation.objects.bulk_create([
+            Reservation(id=1, 
+            guest_information={'name':'김유저', 'email':'user@gmail.com'}, 
+            start_date='2022-07-01', 
+            end_date='2022-07-05',
+            room_id=1, user_id=1),
+            Reservation(id=2, 
+            guest_information={'name':'김유저', 'email':'user@gmail.com'}, 
+            start_date='2022-07-15', 
+            end_date='2022-07-22',
+            room_id=2, user_id=1)
+        ])
+        Review.objects.bulk_create([
+            Review(id=1, content= '별루', rating='2', product_id=1, room_id=1, user_id=1),
+            Review(id=2, content= '좋아요!', rating='4', product_id=1, room_id=1, user_id=1),
+            Review(id=3, content= '또 가고싶음', rating='5', product_id=2, room_id=5, user_id=1),
+            Review(id=4, content= '다시왔다', rating='3', product_id=2, room_id=5, user_id=1)
+        ])
+    # 
+    def tearDown(self):
+        Category.objects.all().delete
+        Region.objects.all().delete
+        Product.objects.all().delete
+# 
+    def test_product_check_in_ascending_post(self):
+        client = Client()
+        response = client.get('/products?start_date=2022-05-01&end_date=2022-05-03&price=200000&sort=check_in-ascending')
+        self.assertEqual(response.json(),
+        {
+            "results": [
+                {
+                    "id": 2,
+                    "name": "호텔2",
+                    "grade": 5,
+                    "address": "호텔2 주소",
+                    "region": "제주도",
+                    "check_in": "12:00",
+                    "check_out": "11:00",
+                    "latitude" : "33.2406820000",
+                    "longtitude" : "126.5317805000",
+                    "category": "호텔",
+                    "main_image": "https://www.daum.net/",
+                    "amanity": [
+                        "수영장"
+                    ],
+                    "avg_rate": 4.0,
+                    "price": {
+                        "min_price": "80000.00",
+                        "max_price": "250000.00",
+                        "low_price": 360000,
+                        "high_price": 500000,
+                        "date": 2
+                    }
+                },
+                {
+                    "id": 1,
+                    "name": "호텔1",
+                    "grade": 5,
+                    "address": "호텔1 주소",
+                    "region": "제주도",
+                    "check_in": "14:00",
+                    "check_out": "11:00",
+                    "category": "호텔",
+                    "main_image": "https://www.naver.com/",
+                    "amanity": [
+                        "수영장"
+                    ],
+                    "avg_rate": 3.0,
+                    "price": {
+                        "min_price": 150000,
+                        "max_price": 300000,
+                        "low_price": 300000,
+                        "high_price": 600000,
+                        "date": 2
+                    }
+                }
+                ]
+        })                
+        self.assertEqual(response.status_code, 200)
+ 
 class ReviewsTest(TestCase):
     def setUp(self):
         User.objects.create(
@@ -434,3 +433,115 @@ class RoomsTest(TestCase):
             ]
         }
         self.assertEqual(response.json(),body)
+
+class ProductTest(TestCase):
+    def setUp(self):
+        user =User.objects.create(
+            name = '위코드',
+            email = 'alsdgkja@natemar.com',
+            kakao_pk = 12397511,
+        )
+
+        ReservationStatus.objects.create(id = 1, status = '123')
+
+        product = Product.objects.create(
+            id=1,
+            name       = '신라호텔',
+            address    = '서울특별시 강남구',
+            check_in   = '15:00',
+            check_out  = '11:00',
+            latitude   = 123.1419517111,
+            longtitude = 60.1231419112,
+        )
+        ProductImage.objects.create(
+            url = 'aaaaaa.png',
+            is_main = True,
+            product=product
+        )
+        Room.objects.create(
+            id =1,
+            name     = '가나다',
+            price    = 100000.00,
+            product  = product,
+            quantity = 2,
+            size     = 20,
+        )
+
+        Room.objects.create(
+            id=2,
+            name     = '가나',
+            price    = 120000.00,
+            product  = product,
+            quantity = 2,
+            size     = 20,
+        )
+
+        Room.objects.create(
+            id=3,
+            name     = '가',
+            price    = 140000.00,
+            product  = product,
+            quantity = 2,
+            size     = 20,
+        )
+
+        Reservation.objects.create(
+            user = user,
+            room_id = 1,
+            start_date = '2022-03-31',
+            end_date = '2022-04-02',
+            guest_information = {'agag' : 'agagag'},
+            reservation_status_id = 1,
+        )
+
+        Review.objects.create(
+            user      = user,
+            product   = product,
+            content   = '',
+            image_url = 'asd',
+            rating    = 3,
+            room_id   = 1
+        )
+
+    def tearDown(self):
+        ReservationStatus.objects.all().delete()
+        Reservation.objects.all().delete()
+        Room.objects.all().delete()
+        Product.objects.all().delete()
+        User.objects.all().delete()
+        Review.objects.all().delete()
+
+    def test_success_product_get(self):
+        client =Client()
+        response = client.get('/products/1?start_date=2022-03-31&end_date=2022-04-02')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(),
+            {
+            'id'           : 1,
+            'name'         : '신라호텔',
+            'grade'        : 0,
+            'address'      : '서울특별시 강남구',
+            'check_in'     : '15:00',
+            'check_out'    : '11:00',
+            'latitude'     : '123.1419517111',
+            'longtitude'   : '60.1231419112',
+            'price'        : 200000,
+            'rating'       : 3.0,
+            'review_count' : 1,
+            'images'       : [
+                {
+                    'url'     : 'aaaaaa.png',
+                    'is_main' : True
+                }
+            ],
+            'is_sold_out' : False
+            }
+        )
+
+    def test_fail_product_get(self):
+        client   = Client()
+        response = client.get('/products/2?start_date=2022-03-31&end_date=2022-04-02')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'message' : 'Invalid Product'}) 
